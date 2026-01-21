cb3fb5e656db757d77ec36e04b88233865296796
IraNye <inye@hchb.com>
2026-01-21 08:30:13 -0500

    add CodeAlong wpf proj

---
+++ CodeAlong/CodeAlong.WPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace CodeAlong.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Style styleSelected;
+        private Style styleUnSelected;
+
+        private Style StyleSelected
+        {
+            get => styleSelected ??= FindResource("ButtonSelected") as Style;
+        }
+
+        private Style StyleUnSelected
+        {
+            get => styleUnSelected ??= FindResource("ButtonUnSelected") as Style;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPatterns_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPatterns.Style = StyleSelected;
+            ButtonTopics.Style = StyleUnSelected;
+        }
+
+        private void ButtonTopics_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPatterns.Style = StyleUnSelected;
+            ButtonTopics.Style = StyleSelected;
+        }
+    }
+}
