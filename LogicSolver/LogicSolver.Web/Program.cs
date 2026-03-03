654fd69af4be411779e6ae109802eca4b844b368
IraNye <inye@hchb.com>
2026-03-03 15:29:16 -0500

    impl and serve index.html

--- LogicSolver/LogicSolver.Web/Program.cs
+++ LogicSolver/LogicSolver.Web/Program.cs
@@ -5,16 +5,21 @@ namespace LogicSolver.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             var app = builder.Build();
 
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
+
             // app.MapGet("/", () => "Hello World!");
-            app.Run(async ctx =>
-            {
-                await ctx.Response.WriteAsJsonAsync(new
-                {
-                    Message = "Hello Logic Solvers!"
-                });
-            });
+            //app.Run(async ctx =>
+            //{
+            //    await ctx.Response.WriteAsJsonAsync(new
+            //    {
+            //        Message = "Hello Logic Solvers!"
+            //    });
+            //});
 
             app.Run();
         }
