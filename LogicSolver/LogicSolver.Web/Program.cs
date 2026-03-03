26c8ecd814d1ef4771e5897fac5cc39c1f9ec741
IraNye <inye@hchb.com>
2026-03-03 15:23:25 -0500

    initial add project

---
+++ LogicSolver/LogicSolver.Web/Program.cs
@@ -0,0 +1,22 @@
+namespace LogicSolver.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            // app.MapGet("/", () => "Hello World!");
+            app.Run(async ctx =>
+            {
+                await ctx.Response.WriteAsJsonAsync(new
+                {
+                    Message = "Hello Logic Solvers!"
+                });
+            });
+
+            app.Run();
+        }
+    }
+}
